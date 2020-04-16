31929ac55aa4cb05dbdb7ff3bfdd47ac9167a00e
TL(Bishnu) <Bishnu@gmail>
2020-04-17 01:24:30 +0530

    Functions.java file is created by TL in function package

---
+++ MyProj1/src/com/nt/function/Functions.java
@@ -0,0 +1,5 @@
+package com.nt.function;
+
+public class Functions {
+
+}
--- MyProj1/src/com/nt/test/Testing.java
+++ MyProj1/src/com/nt/test/Testing.java
@@ -2,7 +2,9 @@ package com.nt.test;
 
 public class Testing {
 	public Testing() {
-		
+		System.out.println("ff1");
+		System.out.println("mr. Bishnu");
+		System.out.println("not to go local");
 	}
 	
 }
